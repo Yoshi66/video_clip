32b945dbb7c5471dc8602ea5891d1d80e596e78e
Yoshiki <yoshiki.mc@gmail.com>
2015-12-30 14:47:25 +0900

    frist commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+require 'streamio-ffmpeg'
+
+class HomeController < ApplicationController
+  def welcome
+    # movie = FFMPEG::Movie.new("https://www.youtube.com/watch?v=QUwxKWT6m7U")
+    # movie.screenshot("tayler.jpg", seek_time: 5)
+  end
+end
